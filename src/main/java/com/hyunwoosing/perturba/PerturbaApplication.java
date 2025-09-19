a7e968483b80b7a9b95fe8ce2bf5be79ccd3d2d7
jwpark <woojang812@gmail.com>
2025-09-19 22:26:07 +0900

    chore: project init

---
+++ src/main/java/com/hyunwoosing/perturba/PerturbaApplication.java
@@ -0,0 +1,13 @@
+package com.hyunwoosing.perturba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerturbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerturbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyunwoosing/perturba/PerturbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunwoosing.perturba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerturbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
