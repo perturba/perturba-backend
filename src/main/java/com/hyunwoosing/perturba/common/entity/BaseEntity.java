61e8885f2b244e4d2c0891022dc73f45e3b23213
jwpark <woojang812@gmail.com>
2025-09-25 19:22:02 +0900

    feat(global): create BaseEntity

---
+++ src/main/java/com/hyunwoosing/perturba/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.hyunwoosing.perturba.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    protected LocalDateTime updatedAt;
+}
