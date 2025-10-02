e6d54eeaf639c56174175ff8e3c603f3c9eeb900
jwpark <woojang812@gmail.com>
2025-10-02 22:24:56 +0900

    feat(jwt): add Jwt Constants

---
+++ src/main/java/com/hyunwoosing/perturba/common/constants/JwtConstants.java
@@ -0,0 +1,9 @@
+package com.hyunwoosing.perturba.common.constants;
+
+
+public final class JwtConstants {
+    private JwtConstants() {}
+
+    public static final String CLAIM_AUTH = "auth";
+    public static final String DEFAULT_ROLE = "ROLE_USER";
+}
