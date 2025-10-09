52bb994099abe6e93f8ef7a7936f5788fe75014e
jwpark <woojang812@gmail.com>
2025-10-09 20:15:20 +0900

    feat(auth): auth Service

--- src/main/java/com/hyunwoosing/perturba/domain/auth/service/AuthService.java
+++ src/main/java/com/hyunwoosing/perturba/domain/auth/service/AuthService.java
@@ -1,23 +1,55 @@
 package com.hyunwoosing.perturba.domain.auth.service;
 
+import com.hyunwoosing.perturba.common.security.jwt.JwtProvider;
 import com.hyunwoosing.perturba.domain.auth.client.google.GoogleOAuthClient;
 import com.hyunwoosing.perturba.domain.auth.client.google.dto.GoogleTokenResponse;
+import com.hyunwoosing.perturba.domain.user.entity.User;
+import com.hyunwoosing.perturba.domain.user.entity.enums.AuthProvider;
+import com.hyunwoosing.perturba.domain.user.entity.enums.UserRole;
+import com.hyunwoosing.perturba.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final GoogleOAuthClient googleClient;
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
-    public String buildGoogleAuthUrl(String codeChallenge, String state, String nonce) {
-        return googleClient.buildAuthUrl(codeChallenge, state, nonce);
+    public User upsertFromGoogleClaims(Map<String, Object> claims) {
+        String email = claims.get("email") != null ? claims.get("email").toString() : null;
+        String name = claims.get("name") != null ? claims.get("name").toString() : email; //name 없으면 email로 대체
+        String picture = claims.get("picture") != null ? claims.get("picture").toString() : null;
+
+        return userRepository.findByEmail(email)
+                .map(u -> {
+                    u.changeProfile(name, picture);
+                    u.markLogin(LocalDateTime.now());
+                    return userRepository.save(u);
+                })
+                .orElseGet(() -> userRepository.save(
+                        User.builder()
+                                .email(email)
+                                .name(name)
+                                .avatarUrl(picture)
+                                .authProvider(AuthProvider.GOOGLE)
+                                .role(UserRole.USER)
+                                .isActive(true)
+                                .build()
+                ));
     }
 
-    public GoogleTokenResponse exchangeCode(String authorizationCode, String codeVerifier) {
-        return googleClient.exchangeAuthorizationCode(authorizationCode, codeVerifier);
+    public String issueAccess(User user) {
+        return jwtProvider.createAccess(user.getId(), user.getEmail());
+    }
+
+    private String val(Object o) {
+        return o != null ? o.toString() : null;
     }
 
 }
