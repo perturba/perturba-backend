5b3743886f48510ed07d37b0bc397b506f1595f1
jwpark <woojang812@gmail.com>
2025-10-09 14:31:36 +0900

    feat(refreshToken): refreshToken repository

---
+++ src/main/java/com/hyunwoosing/perturba/domain/user/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.hyunwoosing.perturba.domain.user.repository;
+
+import com.hyunwoosing.perturba.domain.user.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByTokenHashHexAndRevokedAtIsNull(String tokenHashHex); // revokedAt이 Null인 것 중 tokenHashHex가 일치하는 것 (만료 안된 것)
+}
