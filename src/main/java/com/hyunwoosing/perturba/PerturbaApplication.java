bdc6804855a598b5d181abb3f748ac0b9560883e
jwpark <woojang812@gmail.com>
2025-11-03 14:20:20 +0900

    fix: delete idempotencyProps dependency

--- src/main/java/com/hyunwoosing/perturba/PerturbaApplication.java
+++ src/main/java/com/hyunwoosing/perturba/PerturbaApplication.java
@@ -1,14 +1,13 @@
 package com.hyunwoosing.perturba;
 
 import com.hyunwoosing.perturba.common.config.props.AuthProps;
-import com.hyunwoosing.perturba.common.config.props.IdempotencyProps;
 import com.hyunwoosing.perturba.common.config.props.S3Props;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({AuthProps.class, S3Props.class, IdempotencyProps.class})
+@EnableConfigurationProperties({AuthProps.class, S3Props.class})
 public class PerturbaApplication {
 
 	public static void main(String[] args) {
