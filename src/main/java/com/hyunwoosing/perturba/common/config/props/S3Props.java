9faa5a6db45adadb5534c909d5323d992b30058d
jwpark <woojang812@gmail.com>
2025-11-05 20:22:20 +0900

    chore: upload application.yml

--- src/main/java/com/hyunwoosing/perturba/common/config/props/S3Props.java
+++ src/main/java/com/hyunwoosing/perturba/common/config/props/S3Props.java
@@ -3,7 +3,7 @@ package com.hyunwoosing.perturba.common.config.props;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "storage.s3")
+@ConfigurationProperties(prefix = "perturba.s3")
 public record S3Props(
         String bucket,
         String region,
